61553e111a7af8307c71e8cfaf2a24940ea1eb59
devprashant <kumarjha183@gmail.com>
2025-12-06 23:46:41 +0530

    ts setup & primitive types

---
+++ src/primitiveTypes.ts
@@ -0,0 +1,7 @@
+const name: string = "Prashant Kumar Jha";
+const score: number = 20;
+const isActive: boolean = false;
+
+console.log("Name is: " +name);
+console.log("Score is: " +score);
+console.log("Active State: " +isActive);
