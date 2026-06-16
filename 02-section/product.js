69c6aa68b26fac2e47b527fc279fe92ccce3d339
devprashant <kumarjha183@gmail.com>
2026-06-17 01:36:06 +0530

    compiling ts to js

---
+++ 02-section/product.js
@@ -0,0 +1,4 @@
+"use strict";
+let firstNumber = 23;
+let secondNumber = "a";
+console.log(firstNumber + secondNumber);
---
+++ 02-section/product.ts
@@ -0,0 +1,7 @@
+// Ts will compile this scuuessfully into js file even though it complains and also shows error in terminal
+
+let num1: number = 23;
+let num2: number = "a"; // complains here
+console.log(num1 + num2);
+
+export {};
